31fb3b15fb76b87f0ac53335ff2f013bd2f74cd2
Pratap Singh <aditya.pratap.singh@sap.com>
2017-08-04 09:38:47 +0530

    TryJs First commit

---
+++ ajaxTesting.js
@@ -0,0 +1,12 @@
+//var link="http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b1b15e88fa797225412429c1c50c122a1";
+var link="https://maps.googleapis.com/maps/api/geocode/json?address=%22frazertown%22&key=AIzaSyDtmGvBlqxp2mH9Myu9jWP9RtWLGu1E2tw";
+var connect=new XMLHttpRequest();
+connect.open('GET',link);
+
+//connect.setRequestHeader("Access-Control-Allow-Origin","*");
+//connect.setRequestHeader("Accept","text/html")
+connect.onload=function (){
+    console.log(connect.responseText);
+};
+
+connect.send();
---
+++ funny.js
@@ -0,0 +1,10 @@
+function myFunc() {
+    alert("Hola");
+}
+var testFuncvalue=myFunc();
+alert(testFuncvalue);
+for(var i=0;i<10;i++) { 
+   var name="Aditya";
+    console.log(name);
+    }
+alert(name);
